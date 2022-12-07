d6548872d0875b85af13699442bf6a8775a94982
mohammedlalji <63921121+mohammedlalji@users.noreply.github.com>
2022-12-06 21:00:10 -0500

    Add files via upload

---
+++ Topic.js
@@ -0,0 +1,22 @@
+/*
+ * WEB222 – Final Exam
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ *      Name:       Mohamed Lalji
+ *      Student ID: 168229219
+ *      Date:       2022-12-05
+ */
+
+function otherTopic() {
+  var topic = document.getElementById("other_topic");
+
+  if (document.getElementById("other").checked) {
+    topic.style.display = "inline";
+  } else {
+    topic.style.display = "none";
+  }
+}
